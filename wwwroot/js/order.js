0cbf8cb7c6a8082d87db021c1c387e222d546e92
Brandon Cottrell <bcottrell@my.wctc.edu>
2023-11-30 20:00:21 -0600

    updated order view and order.js

--- wwwroot/js/order.js
+++ wwwroot/js/order.js
@@ -1,15 +1,23 @@
 $(function () {
-    getOrders()
-    function getOrders() {
-      $.getJSON({
-        url: `../../api/order`,
-        success: function (response, textStatus, jqXhr) {
-          console.log(response);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-          // log the error to the console
-          console.log("The following error occured: " + textStatus, errorThrown);
+  getOrders()
+  function getOrders() {
+    $.getJSON({
+      url: `../../api/order`,
+      success: function (response, textStatus, jqXhr) {
+        console.log(response);
+        $('#order_rows').html("");
+        for (var i = 0; i < response.length; i++) {
+        var row = `<tr>
+        <td>${response[i].OrderId}</td>
+        </tr>`;
+          
+          $('#order_rows').append(row);
         }
-      });
-    }
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        // log the error to the console
+        console.log("The following error occured: " + textStatus, errorThrown);
+      }
+    });
+  }
 });
